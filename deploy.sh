735ccddc0c9ffc330ddfdd83bb4413496900c61d
Joao Gabriel Carnevale <jgnunes@amazon.com>
2022-05-27 11:12:23 -0300

    Added wget to .sh file

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@
 # SPDX-License-Identifier: MIT-0
 
 python3 -m pip install simple-term-menu
+wget https://raw.githubusercontent.com/aws-samples/aws-organizations-alternate-contact-manager/main/deploy.sh
 wget https://raw.githubusercontent.com/aws-samples/aws-organizations-alternate-contact-manager/main/script.py
 python3 script.py
